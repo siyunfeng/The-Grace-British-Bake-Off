edf7971a98e61f5ab2df28b334852bb479395bbf
Lauren Baca <laurenbaca1895@gmail.com>
2022-11-08 17:43:44 -0500

    We have an order model

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,4 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const db = require('../db');
 
+const Order = db.define('order', {
+  fulfilled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
 module.exports = Order;
