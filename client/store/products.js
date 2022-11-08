0cede4f8cbd3de5a12f78ee5765262bef541cd21
Siyun Feng <siyunf21@gmail.com>
2022-11-08 17:39:11 -0500

    Updated store/index.js & store/products.js(get all products info)

---
+++ client/store/products.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// Action Type:
+const SET_PRODUCTS = "SET_PRODUCTS";
+
+// Action Creator:
+const _setProducts = (products) => ({ type: SET_PRODUCTS, products });
+
+// Thunk:
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const { data: products } = await axios.get("/api/products");
+      dispatch(_setProducts(products));
+    } catch (error) {
+      console.log("store/allProducts fetchProducts ERROR: ", error);
+    }
+  };
+};
+
+// Initial State:
+const initialState = [];
+
+export default function productsReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products;
+    default:
+      return state;
+  }
+}
