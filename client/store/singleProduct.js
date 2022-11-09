398f8c845d8b8f18d7839ae2e97689b02cae0920
Siyun Feng <siyunf21@gmail.com>
2022-11-09 11:03:54 -0500

    updated decreaseProduct thunk and reducer

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,8 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-const SET_PRODUCT = "SET_PRODUCT";
-const DELETE_PRODUCT = "DELETE_PRODUCT";
-const DECREASE_PRODUCT_QUANTITY = "DECREASE_PRODUCT_QUANTITY";
+const SET_PRODUCT = 'SET_PRODUCT';
+const DECREASE_PRODUCT_QUANTITY = 'DECREASE_PRODUCT_QUANTITY';
 
 const _setProduct = (product) => ({ type: SET_PRODUCT, product });
 const _decreaseProductQuantity = (product) => ({
@@ -16,23 +15,35 @@ export const fetchProduct = (productId) => {
       const { data: product } = await axios.get(`/api/products/${productId}`);
       dispatch(_setProduct(product));
     } catch (error) {
-      console.log("store/singleProduct fetchProduct ERROR: ", error);
+      console.log('store/singleProduct fetchProduct ERROR: ', error);
       throw error;
     }
   };
 };
 
-const initialState = [];
+export const decreaseProduct = (productId) => {
+  return async (dispatch) => {
+    try {
+      const { data: product } = await axios.get(`/api/products/${productId}`);
+      if (product.quantity > 0) {
+        product.quantity -= 1;
+      }
+      dispatch(_decreaseProductQuantity(product));
+    } catch (error) {
+      console.log('store/singleProduct decreaseProduct ERROR: ', error);
+      throw error;
+    }
+  };
+};
+
+const initialState = {};
 
 export default function singleProductReducer(state = initialState, action) {
   switch (action.type) {
     case SET_PRODUCT:
       return action.product;
     case DECREASE_PRODUCT_QUANTITY:
-      if (action.product.quantity > 0) {
-        let updatedQuantity = action.product.quantity - 1;
-        return [...state, { ...action.product, quantity: updatedQuantity }];
-      }
+      return action.product;
     default:
       return state;
   }
