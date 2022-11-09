0f90bf806f7f918a036986140bc2d6ff0551fcd7
Siyun Feng <siyunf21@gmail.com>
2022-11-08 21:53:41 -0500

    Created store/singleProducts.js, still need to work on some of the thunk functions.

---
+++ client/store/singleProduct.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const SET_PRODUCT = "SET_PRODUCT";
+const DELETE_PRODUCT = "DELETE_PRODUCT";
+const DECREASE_PRODUCT_QUANTITY = "DECREASE_PRODUCT_QUANTITY";
+
+const _setProduct = (product) => ({ type: SET_PRODUCT, product });
+const _deleteProduct = (product) => ({ type: DELETE_PRODUCT, product });
+const _decreaseProductQuantity = (product) => ({
+  type: DECREASE_PRODUCT_QUANTITY,
+  product,
+});
+
+export const fetchProduct = (productId) => {
+  return async (dispatch) => {
+    try {
+      const { data: product } = await axios.get(`/api/products/${productId}`);
+      dispatch(_setProduct(product));
+    } catch (error) {
+      console.log("store/singleProduct fetchProduct ERROR: ", error);
+      throw error;
+    }
+  };
+};
+
+const initialState = [];
+
+export default function singleProductReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCT:
+      return action.product;
+    case DELETE_PRODUCT:
+      return state.filter((product) => product.id !== action.product.id);
+    case DECREASE_PRODUCT_QUANTITY:
+      if (action.product.quantity > 0) {
+        let updatedQuantity = action.product.quantity - 1;
+        return [...state, { ...action.product, quantity: updatedQuantity }];
+      }
+    default:
+      return state;
+  }
+}
