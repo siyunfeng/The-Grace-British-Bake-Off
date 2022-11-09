6945f2ff8330f1500c5ee1894e3d699a5397313a
Christine Zheng <zchristine11@gmail.com>
2022-11-09 13:24:15 -0500

    add GET route for all products

--- server/api/products.js
+++ server/api/products.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const { Product } = require('../db');
 
+// GET /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // GET /api/products/:productId
 router.get('/:productId', async (req, res, next) => {
   try {
