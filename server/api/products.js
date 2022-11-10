1f3bd9b7063d714f8fde1a5c5319704db9cfe138
Allie <hw600@nyu.edu>
2022-11-10 15:55:20 -0500

    wrote routes to add/remove/delete user and product & fixed some typos

--- server/api/products.js
+++ server/api/products.js
@@ -23,3 +23,35 @@ router.get('/:productId', async (req, res, next) => {
     next(error);
   }
 });
+
+// POST /api/products
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Product.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/products/:id
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.send(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/products/:id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    rres.send(await product.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const {
-  models: { User },
+  models: { User, Order, Order_Product },
 } = require('../db');
 module.exports = router;
 
+// GET /api/users
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -17,3 +18,56 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
+
+// GET /api/users/:userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const users = await User.findByPk(
+      req.params.userId,
+      {
+        include: {
+          model: Order,
+          include: Order_Product,
+        },
+      },
+      {
+        attributes: ['id', 'username'],
+      }
+    );
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST /api/users
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await User.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/users/:id
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/users/:id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    rres.send(await user.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
