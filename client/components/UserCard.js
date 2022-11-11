959c894633a69e96e4a2291a684e1cfe38691587
Allie <hw600@nyu.edu>
2022-11-11 16:25:57 -0500

    added emojis for allUsers page, felt cute, might delete later

--- client/components/UserCard.js
+++ client/components/UserCard.js
@@ -10,7 +10,22 @@ const UserCard = (props) => {
       <hr></hr>
       <div className="user-detail">
         <p>Email: {user.email || 'N/A'}</p>
-        <p>Role: {user.userType || 'N/A'} </p>
+        <p>
+          Role: {user.userType || 'N/A'}{' '}
+          {user.userType === 'ADMIN' ? (
+            <i
+              class="em em-computer"
+              aria-role="presentation"
+              aria-label="PERSONAL COMPUTER"
+            ></i>
+          ) : (
+            <i
+              class="em em-yum"
+              aria-role="presentation"
+              aria-label="FACE SAVOURING DELICIOUS FOOD"
+            ></i>
+          )}
+        </p>
       </div>
     </div>
   );
