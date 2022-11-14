efbc0573ff65c514fe4fc97067deb8063829543a
Lauren Baca <laurenbaca1895@gmail.com>
2022-11-14 16:02:26 -0500

    Cart component displays appropriate products

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -18,41 +18,49 @@ class Cart extends React.Component {
   }
 
   render() {
-    console.log('props in Cart component: ', this.props);
-
-    const cart = this.props;
+    const { cart } = this.props;
     const { loading } = this.state;
 
-    return (
-      <main>
-        {loading && <p>Loading cart details...</p>}
-        {cart.order_products ? (
-          <div className="cart-layout">
-            {/* <div>Lauren's working on it!</div> */}
-            <div className="cart-products">
-              {cart.order_products.map((op) => (
-                <div key={op.product.id} className="cart-product-layout">
-                  <Link to={`/shop/products/${op.product.id}`}>
-                    <img
-                      className="all-products-img"
-                      src={op.product.imageUrl}
-                    />
-                  </Link>
-                  <div className="product-detail">
-                    <p>{op.product.name}</p>
-                    <p>Number of items: {op.num_items}</p>
-                    <p>Total price from this product: ${op.item_total_price}</p>
+    if (loading) {
+      return (
+        <main>
+          <p>Loading cart details...</p>
+        </main>
+      );
+    } else {
+      const hasCart = cart.order_products.length !== 0;
+
+      return (
+        <main>
+          {hasCart ? (
+            <div className="cart-layout">
+              <div className="cart-products">
+                {cart.order_products.map((op) => (
+                  <div key={op.product.id} className="cart-product-layout">
+                    <Link to={`/shop/products/${op.product.id}`}>
+                      <img
+                        className="all-products-img"
+                        src={op.product.imageUrl}
+                      />
+                    </Link>
+                    <div className="product-detail">
+                      <p>{op.product.name}</p>
+                      <p>Number of items: {op.num_items}</p>
+                      <p>
+                        Total price from this product: ${op.item_total_price}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+              <button className="checkout-button"></button>
             </div>
-            <button className="checkout-button"></button>
-          </div>
-        ) : (
-          <h1>Your cart is empty!!!!</h1>
-        )}
-      </main>
-    );
+          ) : (
+            <h1>Your cart is empty.</h1>
+          )}
+        </main>
+      );
+    }
   }
 }
 
